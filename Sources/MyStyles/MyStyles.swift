9bf0d0ad3c8c1bc5c82dc2fc0ac6b00b3967caf0
Ciarán Mulholland <ciaran@slicelife.com>
2021-10-11 14:13:35 +0100

    adding font registering func

--- Sources/MyStyles/MyStyles.swift
+++ Sources/MyStyles/MyStyles.swift
@@ -1,1 +1,28 @@
-public struct MyStyles { }
+import Foundation
+import UIKit
+import CoreGraphics
+
+public struct MyStyles {
+
+    public static func registerFonts() {
+        CustomFonts.allCases.forEach { font in
+            registerFont(bundle: .module, fontName: font.rawValue, fontExtension: "ttf")
+        }
+    }
+}
+
+fileprivate func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
+    guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
+          let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
+          let font = CGFont(fontDataProvider) else {
+              fatalError("Couldn't create font from filename: \(fontName) with extension \(fontExtension)")
+          }
+
+    var error: Unmanaged<CFError>?
+
+    CTFontManagerRegisterGraphicsFont(font, &error)
+}
+
+enum CustomFonts: String, CaseIterable {
+    case gtAmericaRegular = "gt-america-regular"
+}
