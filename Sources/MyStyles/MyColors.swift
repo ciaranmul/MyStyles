8a2386cdfbfe10a80982ec84311178d3ac5d14f7
Ciarán Mulholland <ciaran@slicelife.com>
2021-10-11 14:29:11 +0100

    specifying bundle .module

--- Sources/MyStyles/MyColors.swift
+++ Sources/MyStyles/MyColors.swift
@@ -44,31 +44,31 @@ private extension MyColor {
 
     // MARK: - Primary Primitives
 
-    static let red1 = Color("red1")
-    static let red2 = Color("red2")
-    static let red3 = Color("red3")
+    static let red1 = Color("red1", bundle: .module)
+    static let red2 = Color("red2", bundle: .module)
+    static let red3 = Color("red3", bundle: .module)
 
-    static let neutral1 = Color("neutral1")
-    static let neutral2 = Color("neutral2")
-    static let neutral3 = Color("neutral3")
-    static let neutral4 = Color("neutral4")
-    static let neutral5 = Color("neutral5")
-    static let neutral6 = Color("neutral6")
-    static let neutral7 = Color("neutral7")
-    static let neutral8 = Color("neutral8")
-    static let neutral9 = Color("neutral9")
+    static let neutral1 = Color("neutral1", bundle: .module)
+    static let neutral2 = Color("neutral2", bundle: .module)
+    static let neutral3 = Color("neutral3", bundle: .module)
+    static let neutral4 = Color("neutral4", bundle: .module)
+    static let neutral5 = Color("neutral5", bundle: .module)
+    static let neutral6 = Color("neutral6", bundle: .module)
+    static let neutral7 = Color("neutral7", bundle: .module)
+    static let neutral8 = Color("neutral8", bundle: .module)
+    static let neutral9 = Color("neutral9", bundle: .module)
 
     // MARK: - Secondary Primitives
 
-    static let yellow1 = Color("yellow1")
-    static let yellow2 = Color("yellow2")
-    static let yellow3 = Color("yellow3")
+    static let yellow1 = Color("yellow1", bundle: .module)
+    static let yellow2 = Color("yellow2", bundle: .module)
+    static let yellow3 = Color("yellow3", bundle: .module)
 
-    static let green1 = Color("green1")
-    static let green2 = Color("green2")
-    static let green3 = Color("green3")
+    static let green1 = Color("green1", bundle: .module)
+    static let green2 = Color("green2", bundle: .module)
+    static let green3 = Color("green3", bundle: .module)
 
-    static let orange1 = Color("orange1")
-    static let orange2 = Color("orange2")
-    static let orange3 = Color("orange3")
+    static let orange1 = Color("orange1", bundle: .module)
+    static let orange2 = Color("orange2", bundle: .module)
+    static let orange3 = Color("orange3", bundle: .module)
 }
