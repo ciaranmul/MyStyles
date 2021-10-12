b5d12fd3f9e5145f032ae45029fc6b9ba1f1093c
Ciarán Mulholland <ciaran@slicelife.com>
2021-10-12 11:22:54 +0100

    font registering rewrite

--- Sources/MyStyles/MyStyles.swift
+++ Sources/MyStyles/MyStyles.swift
@@ -1,29 +1,38 @@
 import Foundation
-import UIKit
+import CoreText
 import CoreGraphics
-
-public struct MyStyles {
-
-    public static func registerFonts() {
-        CustomFonts.allCases.forEach { font in
-            registerFont(bundle: .module, fontName: font.rawValue, fontExtension: "ttf")
-        }
-    }
-}
-
-fileprivate func registerFont(bundle: Bundle, fontName: String, fontExtension: String) {
-    guard let fontURL = bundle.url(forResource: fontName, withExtension: fontExtension),
-          let fontDataProvider = CGDataProvider(url: fontURL as CFURL),
-          let font = CGFont(fontDataProvider) else {
-              fatalError("Couldn't create font from filename: \(fontName) with extension \(fontExtension)")
-          }
-
-    var error: Unmanaged<CFError>?
-
-    CTFontManagerRegisterGraphicsFont(font, &error)
-}
 
 enum CustomFonts: String, CaseIterable {
     case gtAmericaRegular = "gt-america-regular"
     case easyNotes = "easy-notes"
 }
+
+public struct MyStyles {
+
+    public static func registerFonts() {
+        CustomFonts.allCases.forEach { font in
+            registerFont(named: font.rawValue)
+        }
+    }
+
+    static func registerFont(named name: String, ext: String = "ttf") {
+        guard let url = Bundle.module.url(forResource: name, withExtension: ext) else {
+            print("could not find font file: \(name).\(ext)")
+            return
+        }
+        print("registering font at \(url.absoluteString)")
+        guard let fontDataProvider = CGDataProvider(url: url as CFURL) else {
+            print("Could not create font data provider for \(url).")
+            return
+        }
+        guard let font = CGFont(fontDataProvider) else {
+            print("could not create font")
+            return
+        }
+        var error: Unmanaged<CFError>?
+        if !CTFontManagerRegisterGraphicsFont(font, &error) {
+            print(error!.takeUnretainedValue())
+        }
+    }
+}
+
