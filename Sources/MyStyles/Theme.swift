542787d99a5a6d46e0fbca9c6c44e4dc8e56a819
Ryan Grier <ryan.grier@slicelife.com>
2021-10-12 14:04:30 -0400

    Added more theme elements.

--- Sources/MyStyles/Theme.swift
+++ Sources/MyStyles/Theme.swift
@@ -12,8 +12,18 @@ public extension EnvironmentValues {
 }
 
 public protocol Themable {
-    func surfaceColor(for colorScheme: ColorScheme) -> MyColor
-    func backgroundColor(for colorScheme: ColorScheme) -> MyColor
+    // Surfaces
+    func surface(for colorScheme: ColorScheme) -> MyColor
+    func background(for colorScheme: ColorScheme) -> MyColor
+    
+    // Content
+    func content(for colorScheme: ColorScheme) -> MyColor
+    func contentSubtle(for colorScheme: ColorScheme) -> MyColor
+    func contentDisabled(for colorScheme: ColorScheme) -> MyColor
+    func contentSuccess(for colorScheme: ColorScheme) -> MyColor
+    func contentCritical(for colorScheme: ColorScheme) -> MyColor
+    func border(for colorScheme: ColorScheme) -> MyColor
+    func divider(for colorScheme: ColorScheme) -> MyColor
 }
 
 public struct Theme { }
@@ -25,24 +35,80 @@ public extension Theme {
 internal class LegacyTheme { }
 
 extension LegacyTheme: Themable {
-    func surfaceColor(for colorScheme: ColorScheme = .light) -> MyColor {
+    func surface(for colorScheme: ColorScheme = .light) -> MyColor {
         colorScheme.isLightMode ? .neutral1 : .neutral7
     }
     
-    func backgroundColor(for colorScheme: ColorScheme) -> MyColor {
+    func background(for colorScheme: ColorScheme) -> MyColor {
         colorScheme.isLightMode ? .neutral2 : .neutral9
+    }
+    
+    func content(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral9 : .neutral1
+    }
+    
+    func contentSubtle(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral6 : .neutral4
+    }
+    
+    func contentDisabled(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral4 : .neutral6
+    }
+    
+    func contentSuccess(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .green2 : .green2
+    }
+    
+    func contentCritical(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .red2 : .red2
+    }
+    
+    func border(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral3 : .neutral8
+    }
+    
+    func divider(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral3 : .neutral7
     }
 }
 
 internal class CurrentTheme { }
 
 extension CurrentTheme: Themable {
-    func surfaceColor(for colorScheme: ColorScheme = .light) -> MyColor {
+    func surface(for colorScheme: ColorScheme = .light) -> MyColor {
         colorScheme.isLightMode ? .neutral1 : .neutral7
     }
     
-    func backgroundColor(for colorScheme: ColorScheme) -> MyColor {
+    func background(for colorScheme: ColorScheme) -> MyColor {
         colorScheme.isLightMode ? .neutral2 : .neutral9
+    }
+    
+    func content(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral9 : .neutral1
+    }
+    
+    func contentSubtle(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral6 : .neutral4
+    }
+    
+    func contentDisabled(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral4 : .neutral6
+    }
+    
+    func contentSuccess(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .yellow2 : .yellow2
+    }
+    
+    func contentCritical(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .orange2 : .orange2
+    }
+    
+    func border(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral3 : .neutral8
+    }
+    
+    func divider(for colorScheme: ColorScheme) -> MyColor {
+        colorScheme.isLightMode ? .neutral3 : .neutral7
     }
 }
 
