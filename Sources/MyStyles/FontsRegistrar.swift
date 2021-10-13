0b08dc807742b92a290f6c124b6c9db1a56d8f8c
Ciarán Mulholland <ciaran@slicelife.com>
2021-10-13 10:26:01 +0100

    making fonts lazily registered

--- Sources/MyStyles/FontsRegistrar.swift
+++ Sources/MyStyles/FontsRegistrar.swift
@@ -2,41 +2,23 @@ import Foundation
 import CoreGraphics
 import CoreText
 
-private enum CustomFonts: String, CaseIterable {
-    case gtAmericaRegular = "gt-america-regular"
-    case easyNotes = "easy-notes"
-}
-
 protocol CanRegisterFonts {
-    static func registerFonts()
+    static func registerFont(_ font: CustomFont)
 }
 
-public class FontsRegistrar: CanRegisterFonts {
-    public static func registerFonts() {
-        CustomFonts.allCases.forEach { font in
-            registerFont(named: font.rawValue)
-        }
-    }
-
-    static func registerFont(named name: String, ext: String = "ttf") {
-        guard let url = Bundle.module.url(forResource: name, withExtension: ext) else {
-            print("could not find font file: \(name).\(ext)")
-            return
-        }
-
-        guard let fontDataProvider = CGDataProvider(url: url as CFURL) else {
-            print("Could not create font data provider for \(url).")
-            return
-        }
-
-        guard let font = CGFont(fontDataProvider) else {
-            print("could not create font")
+class FontsRegistrar: CanRegisterFonts {
+    static func registerFont(_ font: CustomFont) {
+        guard let url = Bundle.module.url(forResource: font.rawValue, withExtension: font.ext) else {
+            print("could not find font file: \(font.rawValue).\(font.ext)")
             return
         }
 
         var error: Unmanaged<CFError>?
-        if !CTFontManagerRegisterGraphicsFont(font, &error) {
-            guard let error = error else { print("Unknown error occured when registering font: \(font.fullName.debugDescription)"); return }
+        if !CTFontManagerRegisterFontsForURL(url as CFURL, .process, &error) {
+            guard let error = error else {
+                print("Unknown error occured when registering font: \(font.rawValue)")
+                return
+            }
             print(error.takeUnretainedValue())
         }
     }
